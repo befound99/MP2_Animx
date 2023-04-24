615a04e8d0e60c4aa673b05d1da80768a9463731
befound99 <jayvievievie@gmail.com>
2023-04-24 13:57:01 +0800

    previous index

---
+++ assets/script/index-script.js
@@ -0,0 +1,12 @@
+// Buttons
+  // add to cart btn
+  let addToCartBtn = document.querySelector("#addToCartBtn");
+  let buyNowBtn = document.querySelector("#buyNowBtn");
+
+// Events
+  // add to cart event
+  addToCartBtn.addEventListener("click", addToCart);
+
+  function addToCart() {
+    
+  }
