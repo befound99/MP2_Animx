457d034de89dd94cbd07a3d54fb6b594b7dcf0fe
Corinto <salazar72390@gmail.com>
2023-04-24 13:46:44 +0800

    updated cart

--- assets/script/check-out-script.js
+++ assets/script/check-out-script.js
@@ -1,6 +1,8 @@
+// Delivery Note
 const textarea = document.querySelector('#userNote');
 
 textarea.addEventListener('input', () => {
   textarea.rows = 3;
   textarea.rows = Math.ceil(textarea.scrollHeight / 20);
 });
+
