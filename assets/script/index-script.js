e12cca41b7d2e38a38bf4c0fc3aa2f408f2ab440
befound99 <jayvievievie@gmail.com>
2023-04-25 18:33:02 +0800

    v1.3 finished manga card maker

--- assets/script/index-script.js
+++ assets/script/index-script.js
@@ -1,3 +1,88 @@
+//Manga cards maker
+async function fetchMangas(){
+  // fetch .json for mangas
+  const response = await fetch("assets/json/manga-items.json");
+  
+  // Converts .json to .js;
+  const data = await response.json();
+  
+  const mangas = data.mangas;
+  // LOOP
+  mangas.forEach(item => {
+    const mangaSectionBody = document.querySelector(`.manga-section-body`);
+
+    const cardGroup = document.createElement(`div`);
+    cardGroup.classList.add("card-group-manga");
+      const mangaCover = document.createElement('div');
+      mangaCover.classList.add("card-manga");
+      cardGroup.appendChild(mangaCover);
+        const mangaImage = document.createElement(`img`);
+        mangaImage.src = item.image;
+        mangaCover.appendChild(mangaImage);
+        const actionDiv = document.createElement(`div`);
+        actionDiv.classList.add(`action-div`);
+        mangaCover.appendChild(actionDiv);
+          const quickViewContainer = document.createElement(`div`);
+          quickViewContainer.classList.add(`quickview-container`);
+          actionDiv.appendChild(quickViewContainer);
+            const quickViewBtn = document.createElement(`button`);
+            quickViewBtn.innerHTML = `<span>Quick View</span><i class="fa-regular fa-eye"></i>`
+            quickViewBtn.classList.add(`quickview-btn`);
+            quickViewContainer.appendChild(quickViewBtn);
+          const actionsContainer = document.createElement(`div`);
+          actionsContainer.classList.add(`actions-container`);
+          actionsContainer.innerHTML= 
+          `<button class="add-to-cart-btn" title="add to cart"><i class="fa-solid fa-cart-plus"></i></button>
+
+          <button class="add-to-wishlist-btn" title="add to wishlist"><i class="fa-solid fa-heart-circle-plus" ></i></button>
+
+          <button class="buy-now-btn" title="buy now"><i class="fa-regular fa-money-bill-1"></i></button>`
+          actionDiv.appendChild(actionsContainer);
+
+
+
+      const mangaDetails = document.createElement(`div`);
+      mangaDetails.classList.add(`manga-details`);
+      cardGroup.appendChild(mangaDetails);
+        const mangaTitle = document.createElement(`p`);
+        mangaTitle.classList.add(`manga-title`);
+        mangaTitle.innerText = item.title
+        mangaDetails.appendChild(mangaTitle)
+
+        const mangaGenre = document.createElement(`p`);
+        mangaGenre.classList.add(`manga-genre`);
+        mangaGenre.innerText = item.genre
+        mangaDetails.appendChild(mangaGenre)
+
+        const mangaPrice = document.createElement(`span`);
+        mangaPrice.classList.add(`manga-price`);
+        mangaPrice.innerHTML = `₱ ${item.price}`;
+        mangaDetails.appendChild(mangaPrice);
+
+
+    mangaSectionBody.appendChild(cardGroup);
+  })
+}
+fetchMangas()
+
+// Anime Card Maker
+
+fetchAnime = async () => {
+  const response = await fetch('assets/json/anime-items.json');
+  const data = await response.json();
+
+  const animes = data.animes;
+
+  animes.forEach(item => {
+    const animeSectionBody = document.querySelector(".anime-section-body")
+    
+    const animeCardGroup = document.createElement()
+  })
+
+
+} 
+fetchAnime();
+
 /* Actions button */
 
 const carts = document.querySelectorAll('.add-to-cart-btn');
@@ -211,7 +296,6 @@ const increaseQuantity = (tag) => {
     let productContainer = document.querySelector(".products");
     let cartCost = localStorage.getItem('subCost');
   
-    console.log(cartItems);
   
     // If cart items exist and product container is found on the page
     if (cartItems && productContainer) {
@@ -320,7 +404,6 @@ loginModal.style.display = "none";
 };
 
 const total = document.querySelectorAll('.total');
-console.log(total);
 
   
 
